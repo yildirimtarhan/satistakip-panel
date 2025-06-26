29c5c1ec3aa05b7201372487d8cd14ec0d7e7890
yildirimtarhan <yildirimtarhan@gmail.com>
2025-06-26 15:26:24 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>Satış Takip Paneli</h1>
+      <p>Projeniz başarıyla yayınlandı 🎉</p>
+    </div>
+  );
+}
