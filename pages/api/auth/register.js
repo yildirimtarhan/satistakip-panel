95e8b547b38472feb39f4fb3530516fafd08d1fe
Yıldırım Ayluçtarhan <yildirimtarhan@tedarikci.org.tr>
2025-07-17 21:02:02 +0300

    fix: correct path to mongodb.js

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../lib/mongodb";
+import clientPromise from '../../../lib/mongodb';
 import bcrypt from "bcrypt";
 export default async function handler(req, res) {
   if (req.method !== "POST") {
