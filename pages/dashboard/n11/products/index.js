5d0f24b626648f72614795eddf46453a450421c4
Yıldırım Ayluçtarhan <yildirimtarhan@tedarikci.org.tr>
2026-02-22 18:14:54 +0300

    fix: Button import kucuk harf button.js olarak duzeltildi

--- pages/dashboard/n11/products/index.js
+++ pages/dashboard/n11/products/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Button from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 
 /* ===============================
    STATUS BADGE
